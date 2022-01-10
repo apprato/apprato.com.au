8e1ccfc3d9bd9d4f095df0468096fc3cc0878c67
Stephen Goudie <stephen@apprato.com.au>
2022-01-10 13:07:32 +1100

    ap-15: Add padding on categorie cards, add footer

--- src/views/Blog/Blog.js
+++ src/views/Blog/Blog.js
@@ -1,61 +1,48 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Section } from 'components/organisms';
+import React from "react"
+import {makeStyles} from "@material-ui/core/styles"
+import {Section} from "components/organisms"
 import {
   Header,
-  Hero,
+  Categories,
   Experts,
   Readysetcode,
   Consultants,
   Carouselview,
   Partners,
-} from './components';
+} from "./components"
 
-import { sas1, sas2, sas3, sas4, sas5 } from './data';
+import {sas1, sas2, sas3, sas4, sas5} from "./data"
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100%',
-    width: '100%',
+    height: "100%",
+    width: "100%",
   },
   pagePaddingTop: {
     paddingTop: theme.spacing(3),
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up("md")]: {
       paddingTop: theme.spacing(5),
     },
   },
   sectionNoPaddingTop: {
     paddingTop: 0,
   },
-  shape: {
-  },
-}));
+  shape: {},
+}))
 
 const Blog = () => {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <div className={classes.root}>
-        <Section className={classes.pagePaddingTop}>
-          <Header />
-        </Section>
-        <Section className={classes.pagePaddingTop}>
-          <Experts />
-        </Section>
-        <Section className={classes.pagePaddingTop}>
-          <Readysetcode />
-        </Section>
-        <Section className={classes.pagePaddingTop}>
-          <Consultants />
-        </Section>
-        <Section className={classes.pagePaddingTop}>
-          <Carouselview />
-        </Section>
-        <Section>
-          <Partners data1={sas1} data2={sas2} data3={sas3} data4={sas4} data5={sas5} />
-        </Section>
+      <Section className={classes.pagePaddingTop}>
+        <Header />
+      </Section>
+      <Section className={classes.pagePaddingTop}>
+        <Categories />
+      </Section>
     </div>
-  );
-};
+  )
+}
 
-export default Blog;
+export default Blog
