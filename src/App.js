98330488dc07a9ada5fcabcc467ab90ad96768f9
Stephen Goudie <stephen@apprato.com.au>
2022-01-11 14:32:39 +1100

    ap-15: Apollo Client configuration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { Paper, CssBaseline } from '@material-ui/core';
-import AOS from 'aos';
+import React from "react"
+import ApolloClient from "apollo-boost"
+import {ApolloProvider} from "react-apollo"
+import {Router} from "react-router-dom"
+import {createBrowserHistory} from "history"
+import {ThemeProvider} from "@material-ui/core/styles"
+import {Paper, CssBaseline} from "@material-ui/core"
+import AOS from "aos"
 
-import theme from './theme';
-import Routes from './Routes';
+import theme from "./theme"
+import Routes from "./Routes"
 
-import './theme/AktivGrotesk/fonts.css';
-import 'react-lazy-load-image-component/src/effects/opacity.css';
-import 'aos/dist/aos.css';
+import "./theme/AktivGrotesk/fonts.css"
+import "react-lazy-load-image-component/src/effects/opacity.css"
+import "aos/dist/aos.css"
 
-const browserHistory = createBrowserHistory();
+const browserHistory = createBrowserHistory()
 
-browserHistory.listen(location => {
+browserHistory.listen((location) => {
   // Use setTimeout to make sure this runs after React Router's own listener
   setTimeout(() => {
     // Keep default behavior of restoring scroll position when user:
@@ -22,33 +24,39 @@ browserHistory.listen(location => {
     // - clicked on a link that programmatically calls `history.goBack()`
     // - manually changed the URL in the address bar (here we might want
     // to scroll to top, but we can't differentiate it from the others)
-    if (location.action === 'POP') {
-      return;
+    if (location.action === "POP") {
+      return
     }
     // In all other cases, scroll to top
-    window.scrollTo(0, 0);
-  });
-});
+    window.scrollTo(0, 0)
+  })
+})
 
 const App = () => {
   AOS.init({
     once: true,
     delay: 50,
     duration: 500,
-    easing: 'ease-in-out',
-  });
+    easing: "ease-in-out",
+  })
+
+  const client = new ApolloClient({
+    uri: "http://3.24.116.71/graphql",
+  })
 
   return (
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <Paper>
-        <Router history={browserHistory}>
-          <Routes />
-        </Router>
-      </Paper>
-    </ThemeProvider>
-  );
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Paper>
+          <Router history={browserHistory}>
+            <Routes />
+          </Router>
+        </Paper>
+      </ThemeProvider>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
