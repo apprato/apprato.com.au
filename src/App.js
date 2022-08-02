88fa7a1b5bbf5caa7b97aac8559f88534a890b4b
Ubuntu <ubuntu@ip-172-31-42-30.ap-southeast-2.compute.internal>
2022-08-02 03:13:14 +0000

    Update graphqland uploads directory to backend.apprato.com.au

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ const App = () => {
   })
 
   const client = new ApolloClient({
-    uri: "http://3.24.116.71/graphql",
+    uri: "https://backend.apprato.com.au/graphql",
     cache: new InMemoryCache(),
   })
 
